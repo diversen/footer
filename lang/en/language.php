ab928f10b292aaf8f8f3a7d3dd42822790f477ef
Dennis Iversen <dennis.iversen@gmail.com>
2016-03-02 18:02:24 +0100

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -5,6 +5,7 @@ $LANG = array();
 // Translation of file modules/footer/install.inc
 
 $LANG['Footer license'] = '2014. Unauthorized use and/or duplication of this material without express and written permission from this author and/or owner is strictly prohibited. Excerpts and links may be used, provided that full and clear credit is given to this site, with appropriate and specific direction to the original content.';
+
 // Translation of file modules/footer/views.php
 
 $LANG['Footer license'] = '2014. Unauthorized use and/or duplication of this material without express and written permission from this author and/or owner is strictly prohibited. Excerpts and links may be used, provided that full and clear credit is given to this site, with appropriate and specific direction to the original content.';
